94e9b8b17acd24d4b2fe045b76fd5114c147febb
Asmaa Hassan <asmaahassanhamza1@gmail.com>
2024-11-04 13:18:47 +0200

    make first web component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { SystemLayoutComponent } from './app/Shared-Layout/system-layout.component';
+import 'zone.js';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+createApplication()
+    .then((app) => {
+        const sharedLayout = createCustomElement(SystemLayoutComponent, { injector: app.injector });
+        customElements.define('shared-layout', sharedLayout);
+    })
+    .catch((err) => console.error(err));
